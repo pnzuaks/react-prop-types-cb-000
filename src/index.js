54cadb26bd09e5f8fda3afa060d4e4b0656b311d
Patricia Nykaza <pnykaza@calstatela.edu>
2019-02-08 22:27:49 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -26,5 +26,7 @@ Order.propTypes = {
   cone: PropTypes.bool,
   size: PropTypes.string,
   scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
-  orderInfo: PropTypes.object.isRequired
+  orderInfo: PropTypes.shape({
+    
+  })
 };
